fd09a671acb5f71c9eb2b80d67b7558582f0ecba
Trung Nguyen <86908083+qtrunguyen@users.noreply.github.com>
2024-02-25 10:12:44 -0500

    Websocket connection

--- main.go
+++ main.go
@@ -3,13 +3,75 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"net/http"
 
 	service "meetcode-backend/service"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 	_ "github.com/lib/pq"
 )
+
+type Message struct {
+	Text string `json:"text"`
+}
+
+var (
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+
+	clients   = make(map[*websocket.Conn]bool)
+	broadcast = make(chan Message)
+)
+
+func handleWebSocket(c *gin.Context) {
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	// Add client to map
+	clients[conn] = true
+
+	// Infinite loop to read incoming messages
+	for {
+		var msg Message
+		err := conn.ReadJSON(&msg)
+		if err != nil {
+			log.Println(err)
+			delete(clients, conn)
+			return
+		}
+
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		// Get next message from broadcast channel
+		msg := <-broadcast
+
+		// Send message to all clients
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
 
 func main() {
 	db, err := sql.Open("postgres", "user=postgres dbname=postgres sslmode=disable password=123456 host=localhost")
@@ -30,6 +92,10 @@ func main() {
 
 	// Route to handle user log-in
 	router.POST("/login", userService.LogIn)
+
+	router.GET("/ws", handleWebSocket)
+
+	go handleMessages()
 
 	// Start the server
 	port := 8080 // You can change the port as needed
